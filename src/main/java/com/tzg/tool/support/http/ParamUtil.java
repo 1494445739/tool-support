1e331a43640535eedfbe0def3d65a117aa6bb626
zenglin <zengl@tzg.cn>
2017-08-29 18:39:56 +0800

    add ParamUtil

---
+++ src/main/java/com/tzg/tool/support/http/ParamUtil.java
@@ -0,0 +1,18 @@
+package com.tzg.tool.support.http;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.tzg.tool.support.io.IOUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+
+public final class ParamUtil {
+
+    public JSONObject getJSONFromRequest( HttpServletRequest request ) throws Exception {
+        InputStream is          = request.getInputStream();
+        String      paramString = IOUtil.convertStream2String( is );
+        return JSON.parseObject( paramString );
+    }
+
+}
